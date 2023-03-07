a86a9e899f9bc3ac81be9fe402b44262438c91b9
luydjmix2 <luydjmix@hotmail.com>
2023-03-07 16:32:04 -0500

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -8,14 +8,29 @@ def dibujar_laberinto():
                 pantalla = "" + pantalla + "#"
         pantalla = "" + pantalla + "\n"
     basic.show_string(pantalla)
-
-pos_x = 0
-pos_y = 0
+def mover_derecha():
+    global pos_x
+    if pos_x < 4 and laberinto[pos_y][pos_x + 1] == 0:
+        pos_x += 1
+def mover_arriba():
+    global pos_y
+    if pos_y > 0 and laberinto[pos_y - 1][pos_x] == 0:
+        pos_y += 0 - 1
+def mover_abajo():
+    global pos_y
+    if pos_y < 4 and laberinto[pos_y + 1][pos_x] == 0:
+        pos_y += 1
+def mover_izquierda():
+    global pos_x
+    if pos_x > 0 and laberinto[pos_y][pos_x - 1] == 0:
+        pos_x += 0 - 1
 pantalla = ""
+pos_y = 0
+pos_x = 0
 laberinto: List[List[number]] = []
-x = 0
-y = 0
 k = 0
+y = 0
+x = 0
 laberinto = [[1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1],
     [1, 0, 1, 0, 1],
@@ -23,22 +38,15 @@ laberinto = [[1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1]]
 while True:
     dibujar_laberinto()
-    def mover_izquierda():
-        global pos_x
-        if pos_x > 0 and laberinto[pos_y][pos_x-1] == 0:
-            pos_x -= 1
-
-    def mover_derecha():
-        global pos_x
-        if pos_x < 4 and laberinto[pos_y][pos_x+1] == 0:
-            pos_x += 1
-
-    def mover_arriba():
-        global pos_y
-        if pos_y > 0 and laberinto[pos_y-1][pos_x] == 0:
-            pos_y -= 1
-
-    def mover_abajo():
-        global pos_y
-        if pos_y < 4 and laberinto[pos_y+1][pos_x] == 0:
-            pos_y += 1
+    if pos_x == 4 and pos_y == 4:
+        basic.show_string("¡Ganaste!")
+        pos_x = 0
+        pos_y = 0
+    
+    def on_button_pressed_a():
+        mover_izquierda()
+    input.on_button_pressed(Button.A, on_button_pressed_a)
+    def on_button_pressed_b():
+        mover_derecha()
+    input.on_button_pressed(Button.B, on_button_pressed_b)
+    
--- main.ts
+++ main.ts
@@ -14,46 +14,58 @@ function dibujar_laberinto() {
     basic.showString(pantalla)
 }
 
-let pos_x = 0
-let pos_y = 0
+function mover_derecha() {
+    
+    if (pos_x < 4 && laberinto[pos_y][pos_x + 1] == 0) {
+        pos_x += 1
+    }
+    
+}
+
+function mover_arriba() {
+    
+    if (pos_y > 0 && laberinto[pos_y - 1][pos_x] == 0) {
+        pos_y += 0 - 1
+    }
+    
+}
+
+function mover_abajo() {
+    
+    if (pos_y < 4 && laberinto[pos_y + 1][pos_x] == 0) {
+        pos_y += 1
+    }
+    
+}
+
+function mover_izquierda() {
+    
+    if (pos_x > 0 && laberinto[pos_y][pos_x - 1] == 0) {
+        pos_x += 0 - 1
+    }
+    
+}
+
 let pantalla = ""
+let pos_y = 0
+let pos_x = 0
 let laberinto : number[][] = []
-let x = 0
-let y = 0
 let k = 0
+let y = 0
+let x = 0
 laberinto = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 1, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]]
 while (true) {
     dibujar_laberinto()
-    function mover_izquierda() {
-        
-        if (pos_x > 0 && laberinto[pos_y][pos_x - 1] == 0) {
-            pos_x -= 1
-        }
-        
+    if (pos_x == 4 && pos_y == 4) {
+        basic.showString("¡Ganaste!")
+        pos_x = 0
+        pos_y = 0
     }
     
-    function mover_derecha() {
-        
-        if (pos_x < 4 && laberinto[pos_y][pos_x + 1] == 0) {
-            pos_x += 1
-        }
-        
-    }
-    
-    function mover_arriba() {
-        
-        if (pos_y > 0 && laberinto[pos_y - 1][pos_x] == 0) {
-            pos_y -= 1
-        }
-        
-    }
-    
-    function mover_abajo() {
-        
-        if (pos_y < 4 && laberinto[pos_y + 1][pos_x] == 0) {
-            pos_y += 1
-        }
-        
-    }
-    
+    input.onButtonPressed(Button.A, function on_button_pressed_a() {
+        mover_izquierda()
+    })
+    input.onButtonPressed(Button.B, function on_button_pressed_b() {
+        mover_derecha()
+    })
 }
