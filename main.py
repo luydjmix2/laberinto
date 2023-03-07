558bc82fa99d70537ab405936ffc573649c03e9c
luydjmix2 <luydjmix@hotmail.com>
2023-03-07 16:15:03 -0500

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 def dibujar_laberinto():
-    for index in range(len(laberinto)):
-        print(".")
+    global pantalla
+    for i in range(5):
+        for j in range(5):
+            if laberinto[i][j] == 1:
+                pantalla = "" + pantalla + "."
+            else:
+                pantalla = "" + pantalla + "#"
+        pantalla = "" + pantalla + "\n"
+    basic.show_string(pantalla)
 
-def on_button_pressed_a():
-    dibujar_laberinto()
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
+pos_x = 0
+pos_y = 0
+pantalla = ""
 laberinto: List[List[number]] = []
-y = 0
 x = 0
+y = 0
+k = 0
 laberinto = [[1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1],
     [1, 0, 1, 0, 1],
     [1, 0, 0, 0, 1],
     [1, 1, 1, 1, 1]]
+while True:
+    dibujar_laberinto()
+    def mover_izquierda():
+        global pos_x
+        if pos_x > 0 and laberinto[pos_y][pos_x-1] == 0:
+            pos_x -= 1
+
+    def mover_derecha():
+        global pos_x
+        if pos_x < 4 and laberinto[pos_y][pos_x+1] == 0:
+            pos_x += 1
+
+    def mover_arriba():
+        global pos_y
+        if pos_y > 0 and laberinto[pos_y-1][pos_x] == 0:
+            pos_y -= 1
+
+    def mover_abajo():
+        global pos_y
+        if pos_y < 4 and laberinto[pos_y+1][pos_x] == 0:
+            pos_y += 1
--- main.ts
+++ main.ts
@@ -1,13 +1,59 @@
 function dibujar_laberinto() {
-    for (let index = 0; index < laberinto.length; index++) {
-        console.log(".")
+    
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            if (laberinto[i][j] == 1) {
+                pantalla = "" + pantalla + "."
+            } else {
+                pantalla = "" + pantalla + "#"
+            }
+            
+        }
+        pantalla = "" + pantalla + "\n"
     }
+    basic.showString(pantalla)
 }
 
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    dibujar_laberinto()
-})
+let pos_x = 0
+let pos_y = 0
+let pantalla = ""
 let laberinto : number[][] = []
-let y = 0
 let x = 0
+let y = 0
+let k = 0
 laberinto = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 1, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]]
+while (true) {
+    dibujar_laberinto()
+    function mover_izquierda() {
+        
+        if (pos_x > 0 && laberinto[pos_y][pos_x - 1] == 0) {
+            pos_x -= 1
+        }
+        
+    }
+    
+    function mover_derecha() {
+        
+        if (pos_x < 4 && laberinto[pos_y][pos_x + 1] == 0) {
+            pos_x += 1
+        }
+        
+    }
+    
+    function mover_arriba() {
+        
+        if (pos_y > 0 && laberinto[pos_y - 1][pos_x] == 0) {
+            pos_y -= 1
+        }
+        
+    }
+    
+    function mover_abajo() {
+        
+        if (pos_y < 4 && laberinto[pos_y + 1][pos_x] == 0) {
+            pos_y += 1
+        }
+        
+    }
+    
+}
